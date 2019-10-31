beb5b8e042984588ef44a33815aa7ea9d885ac80
qs_lll@163.com <l124578>
2019-10-31 16:25:12 +0800

    Create install_ss.sh

---
+++ install_ss.sh
@@ -0,0 +1,3 @@
+apt-get update
+apt-get install docker.io
+sudo bash -c "$(wget -qO- https://raw.githubusercontent.com/Jigsaw-Code/outline-server/master/src/server_manager/install_scripts/install_server.sh)"
